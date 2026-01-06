3a1dc7927bd266ed5d83de5fc7b427f9a8b4cbc0
Thiago Vitor Simão <T50140@se.df.gov.br>
2026-01-06 15:19:40 -0300

    Config: Protegendo chaves sensíveis

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@eh2%5w)ekzwct30_$d253)#e!7abgqtj3_&&9niwc6sl2)(r_'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-chave-padrao-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
